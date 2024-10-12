2438754e78458ad1396d126d14a1db8be59540b7
zanafancy <zanafancy@gmail.com>
2024-10-12 14:50:41 +0100

    updated video

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# dropbox: storage space
+// github: storage space 
+// git -v (to know if you have git installed)
+// ensure you are at the current directory, before you initialize your entire computer.
+// pwd: print working directory, to confirm where you are, if you are in the right folder
+// git init: to creat a folder that git will be using to track everything with branches
+// git add: to add all the files instead of typing manually. use the . it represents every untracked file (git add .)
+// commit: to add it to git so that git will know the file: git commit -m "first commit"
+// git config --global user.email "zanafancy@gmail.com"
+// git config --global user.name "zanafancy"
+// rm -rf . git: to remove a file or folder from commit
+// touch README.md: to create a readme file
+
+<!-- branch: folder you are creating to keep track of things e.g master. etc -->
--- object.js
+++ object.js
@@ -13,7 +13,7 @@ const person =
         email: "zanafancy@gmail.com",
     },
     images: ["file.png", "file2.png"],
-    videos: [...],
+    videos: ["song.mp4"],
 };
 
 const car = {
@@ -43,15 +43,3 @@ console.log(person.firstName);
 console.log(person.lastName);
 console.log(person.age);
 console.log(person.gender);
-
-// dropbox: storage space
-// github: storage space 
-// git -v (to know if you have git installed)
-// ensure you are at the current directory, before you initialize your entire computer.
-// pwd: print working directory, to confirm where you are, if you are in the right folder
-// git init: to creat a folder that git will be using to track everything with branches
-// git add: to add all the files instead of typing manually. use the . it represents every untracked file (git add .)
-// commit: to add it to git so that git will know the file: git commit -m "first commit"
-// git config --global user.email "zanafancy@gmail.com"
-// git config --global user.name "zanafancy"
-// rm -rf . git:
